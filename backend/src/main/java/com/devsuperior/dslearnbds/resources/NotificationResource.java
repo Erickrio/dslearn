f7bb593ebe737ba1537d15ca9ce0be73af4fef09
Erick Monteiro <monteiro.ck@hotmail.com>
2022-12-06 11:09:34 -0300

    search for notification  which may be unread

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/notifications")
@@ -20,8 +17,11 @@ public class NotificationResource {
     private NotificationService service;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
-        Page<NotificationDTO> page = service.notificationForCurrentUser(pageable);
+    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(
+            @RequestParam(name="unreadOnly",defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable){
+
+        Page<NotificationDTO> page = service.notificationForCurrentUser(unreadOnly,pageable);
         return ResponseEntity.ok().body(page);
     }
 
